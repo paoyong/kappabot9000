c7264fb05da8cf0f773c194e54c5f77be192f52e
Keith Yong <terda12@gmail.com>
2015-01-29 16:40:19 -0500

    Added occurrences.js

---
+++ occurrences.js
@@ -0,0 +1,15 @@
+module.exports = function (string, subString, allowOverlapping) {
+    string+=""; subString+="";
+    if(subString.length<=0) return string.length+1;
+
+    var n=0, pos=0;
+    var step=(allowOverlapping)?(1):(subString.length);
+
+    while(true){
+        pos=string.indexOf(subString,pos);
+        if(pos>=0){ n++; pos+=step; } else break;
+    }
+    return(n);
+    
+}
+
